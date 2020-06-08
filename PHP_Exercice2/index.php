ccdeef8c0f4d837cf3b093deace5a2e7d2315097
unknown <jules.gioux@gmail.com>
2020-06-08 12:07:11 +0200

    ex2

---
+++ PHP_Exercice2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Partie 7 Exercice 2</title>
+</head>
+<body>
+    <form action="user.php" method="post">
+    <fieldset>
+            <legend>Ajouter un utilisateur :</legend>
+            <label for="fName">Votre prénom :</label>
+            <input type="text" id="fName" name="fName" maxlength="50" required="required" pattern="[^\n\r0-9_!¡?÷?¿\/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,50}" title="Not a valid name !">
+            <label for="lName">Votre nom :</label>
+            <input type="text" id="lName" name="lName" maxlength="50" required="required" pattern="[^\n\r0-9_!¡?÷?¿\/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,50}" title="Not a valid name !">
+            <input type="submit" value="Envoyer">
+    </fieldset>
+    </form>
+</body>
+</html>
